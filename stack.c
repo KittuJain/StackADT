b4bdf9145629ae88dabcac2a7dc8de314e116e7d
Kittu Jain <cskratijain@gmail.com>
2015-02-01 17:45:10 +0530

    completed implementation of pop functionality added more tests for push in stack version 1 completed

--- stack.c
+++ stack.c
@@ -14,3 +14,8 @@ int push (Stack stack, void* data){
 	int count = add_to_list(stack.list,node);
 	return (count < 0) ? -1 : stack.list->count;
 }
+
+void* pop (Stack stack){
+	int count = stack.list->count;
+	return (count == 0) ? (void*)(-1) : deleteElementAt(stack.list, count - 1);
+}
