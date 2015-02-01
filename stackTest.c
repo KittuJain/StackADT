0a355484f61d78f59ec6bd49097257ce754a9b2f
Kittu Jain <cskratijain@gmail.com>
2015-02-01 17:52:00 +0530

    added test for negative result when their is no element on stack

--- stackTest.c
+++ stackTest.c
@@ -72,3 +72,11 @@ void test_pop_deletes_the_topmost_element_from_the_stack (){
 	assertEqual(stack.list->count, 1);
 	free(stack.list);
 }
+
+void test_pop_returns_minus_1_when_there_is_no_element_on_stack (){
+	Stack stack = createStack();
+	assertEqual(stack.list->count, 0);
+	assert((int*)pop(stack)==-1);
+	assertEqual(stack.list->count, 0);
+	free(stack.list);
+}
